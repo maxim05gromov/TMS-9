915079551c2af808761ced8eb5dd90dfc6cb4edd
Maksim <i@mgromov.ru>
2024-07-21 21:22:48 +0300

    first commit

---
+++ HW9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW9
+//
+//  Created by Максим Громов on 14.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
